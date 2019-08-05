596b394ff906a99514593fac70683ec6a6714c2e
jon alden <jondevalden@gmail.com>
2019-08-04 22:32:07 -0500

    pseudo coded the javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+//make images a button
+
+//assign each button a different random number, 1 - 12
+   
+//Don't show the value of each button
+    
+//make the id=total-score display the sum of all button presses 
+
+//assign the id=random-number a random number, 19 - 120 and display it
+
+//if id=total-score equals random number, alert winner, wins++, and assign new random numbers
+   
+/*if id=total-score is higher than random number, alert loser, losses++, 
+    and assign new random numbers*/
+
+    
+
+
+
