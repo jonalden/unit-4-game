12f505433b30a85725525191e9bd1b0b2ab3c4d7
jon alden <jondevalden@gmail.com>
2019-08-05 15:50:49 -0500

    added functionality to randomly assigned goal number, and character buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,40 @@
+//Make variables for win/loss counters
 
-//make images a button
+let wins = 0;
+let losses = 0;
+let goalNumber = randomNumber();
 
 //assign each button a different random number, 1 - 12
-   
-//Don't show the value of each button
+const akuma = document.getElementById("akuma");
+const ken = document.getElementById("ken");
+const chunLi = document.getElementById("chun-li");
+const bison = document.getElementById("bison");
+
+function randomButtonNumber() {
+    return(Math.floor(Math.random() * 12) + 1);
+}
+
+ken.setAttribute("value", randomButtonNumber());
+akuma.setAttribute("value", randomButtonNumber());
+chunLi.setAttribute("value", randomButtonNumber());
+bison.setAttribute("value", randomButtonNumber());
+
+//create an onClick event listener for each button 
     
 //make the id=total-score display the sum of all button presses 
 
 //assign the id=random-number a random number, 19 - 120 and display it
+
+const randomNumberHTML = document.getElementById("random-number");
+
+function randomNumber() {
+    return(Math.floor(Math.random() * 101) + 19);
+}
+
+console.log(goalNumber);
+
+randomNumberHTML.textContent = goalNumber;
+
 
 //if id=total-score equals random number, alert winner, wins++, and assign new random numbers
    
